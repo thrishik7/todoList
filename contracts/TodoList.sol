9fdbc09944122b266ff6aa1909f01e92396b6026
thrishik7 <thrishik.s@gmail.com>
2020-05-02 20:42:06 +0530

    rendering tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,5 +4,22 @@ contract TodoList{
 //statevariables
 uint public taskCount=0;
 
+struct Task{
+    uint id;
+    string content;
+    bool completed;
+}
+
+mapping(uint => Task) public tasks;
+
+constructor() public {
+    createTask("Check out this");
+}
+
+function createTask(string memory _content) public {
+taskCount ++;
+tasks[taskCount]= Task(taskCount,_content, false);
+}
+
 }
 
