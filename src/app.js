d884002b44b2befe03a846284a7846782a2a7532
thrishik7 <thrishik.s@gmail.com>
2020-05-04 01:25:42 +0530

    creating task

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ App={
     loading:false,
     contracts:{},
     load: async() => {
-       await App.loadWeb3()
-       await App.loadAccount()
-       await App.loadContract()
-       await App.render()
+    await App.loadWeb3()
+    await App.loadAccount()
+    await App.loadContract()
+    await App.render()
     },
 
     loadWeb3: async () => {
@@ -17,15 +17,15 @@ App={
     }
     // Modern dapp browsers...
     if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
-      try {
+    window.web3 = new Web3(ethereum)
+    try {
         // Request account access if needed
-        await ethereum.enable()
+    await ethereum.enable()
         // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
-      } catch (error) {
+    web3.eth.sendTransaction({/* ... */})
+    } catch (error) {
         // User denied account access...
-      }
+    }
     }
     // Legacy dapp browsers...
     else if (window.web3) {
@@ -38,7 +38,7 @@ App={
     else {
     console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
-  },  
+    },  
         loadAccount: async () =>{
         App.account = web3.eth.accounts[0];
         },
@@ -58,12 +58,42 @@ App={
     App.setLoading(true)
     //Render Account
     $('#account').html(App.account)
+    
+    await App.renderTasks()
+
     App.setLoading(false)
 
     },
-    renderTasks: async () =>{
+
+    renderTasks: async() =>{
+      //load the total task count from blockchain
+      const taskCount = await App.todoList.taskCount()
+      const $taskTemplate = $('.taskTemplate')
+      //Render out each task with a new task template
+      for(var i=1; i<=taskCount; i++){
+       const task = await App.todoList.tasks(i)
+       const taskId = task[0].toNumber()
+       const taskContent = task[1]
+       const taskCompleted = task[2]
+
+       const $newTaskTemplate = $taskTemplate.clone()
+       $newTaskTemplate.find('.content').html(taskContent)
+       $newTaskTemplate.find('input')
+                       .prop('name', taskId)
+                       .prop('checked', taskCompleted)
+                    //   .on('click', App.toggleCompleted)
+       
+       if (taskCompleted)
+         $('#completedTaskList').append($newTaskTemplate)
+       else
+         $('#taskList').append($newTaskTemplate)
     
-    }
+      
+      //show the task
+      $newTaskTemplate.show()
+      }
+    },
+
     setLoading: (boolean) =>{
         App.loading = boolean
         const loader = $('#loader')
